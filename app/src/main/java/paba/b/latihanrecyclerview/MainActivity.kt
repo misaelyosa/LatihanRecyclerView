ac86745ab5aef111b5404a4da3ac7d1f200df3e9
Misael Yosa <107090127+misaelyosa@users.noreply.github.com>
2024-11-20 19:14:43 +0700

    done 11

--- app/src/main/java/paba/b/latihanrecyclerview/MainActivity.kt
+++ app/src/main/java/paba/b/latihanrecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package paba.b.latihanrecyclerview
 
 import android.content.DialogInterface
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,18 +29,39 @@ class MainActivity : AppCompatActivity() {
         }
 
         _rvWayang = findViewById<RecyclerView>(R.id.rvWayang)
-        SiapkanData()
+
+        sp = getSharedPreferences("dataSP", MODE_PRIVATE)
+
+        val gson = Gson()
+        val isiSP = sp.getString("spWayang", null)
+        val type = object : TypeToken<ArrayList<wayang>> (){}.type
+        if (isiSP!=null)
+            arWayang = gson.fromJson(isiSP, type)
+        if (arWayang.size == 0){
+            SiapkanData()
+        } else {
+            arWayang.forEach{
+                _nama.add(it.nama)
+                _gambar.add(it.foto)
+                _deskripsi.add(it.deskripsi)
+                _karakter.add(it.karakter)
+            }
+            arWayang.clear()
+        }
+
         TambahData()
         TampilkanData()
     }
 
-    private lateinit var _nama : MutableList<String>
-    private lateinit var _karakter : MutableList<String>
-    private lateinit var _deskripsi : MutableList<String>
-    private lateinit var _gambar : MutableList<String>
+    private  var _nama : MutableList<String> = emptyList<String>().toMutableList()
+    private  var _karakter : MutableList<String> = emptyList<String>().toMutableList()
+    private  var _deskripsi : MutableList<String> = emptyList<String>().toMutableList()
+    private  var _gambar : MutableList<String> = emptyList<String>().toMutableList()
     private var arWayang = arrayListOf<wayang>()
 
     private lateinit var _rvWayang : RecyclerView
+
+    lateinit var sp : SharedPreferences
 
     fun SiapkanData() {
         _nama = resources.getStringArray(R.array.namaWayang).toMutableList()
@@ -47,6 +71,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun TambahData() {
+        val gson = Gson()
+        val editor = sp.edit()
         arWayang.clear()
         for (position in _nama.indices) {
             val data = wayang(
@@ -57,6 +83,9 @@ class MainActivity : AppCompatActivity() {
             )
             arWayang.add(data)
         }
+        val json = gson.toJson(arWayang)
+        editor.putString("spWayang", json)
+        editor.apply()
     }
 
     fun TampilkanData(){
